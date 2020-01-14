c43c59899222e9124336b348338ac7e88b5531ed
davidnguyen179 <dzungnguyen179@gmail.com>
2020-01-14 11:30:00 +0900

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from './components/button';
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Button text="click me" />
+      </div>
+    );
+  }
+}
